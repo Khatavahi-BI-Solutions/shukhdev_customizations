c467d62c588d592e04b7b3eb68cf35d186d11532
Jigar Tarpara <jigartarpara68@gmail.com>
2023-02-16 14:55:49 +0530

    Cages

--- shukhdev_customizations/shukhdev_customizations/doctype/employee_payroll_entry/employee_payroll_entry.js
+++ shukhdev_customizations/shukhdev_customizations/doctype/employee_payroll_entry/employee_payroll_entry.js
@@ -1,8 +1,121 @@
 // Copyright (c) 2023, Jigar Tarpara and contributors
 // For license information, please see license.txt
-
+var in_progress = false;
 frappe.ui.form.on('Employee Payroll Entry', {
-	// refresh: function(frm) {
+	refresh: function (frm) {
+		frm.page.clear_primary_action();
+		frm.add_custom_button(__("Create Payroll Entry"),
+			function () {
+				frm.events.create_payroll_entry(frm);
+			}
+		).toggleClass("btn-primary", false);
+	},
+	create_payroll_entry: function (frm) {
+		return frappe.call({
+			doc: frm.doc,
+			method: 'create_payroll_entry',
+		}).then(r => {
+			if (r.docs && r.docs[0].employees) {
+				frm.employees = r.docs[0].employees;
+				frm.dirty();
+				frm.save();
+				frm.refresh();
+				if (r.docs[0].validate_attendance) {
+					render_employee_attendance(frm, r.message);
+				}
+				frm.scroll_to_field("employees");
+			}
+		});
+	},
+	start_date: function (frm) {
+		if (!in_progress && frm.doc.start_date) {
+			frm.trigger("set_end_date");
+		} else {
+			// reset flag
+			in_progress = false;
+		}
+		// frm.events.clear_employee_table(frm);
+	},
+	set_end_date: function (frm) {
+		frappe.call({
+			method: 'hrms.payroll.doctype.payroll_entry.payroll_entry.get_end_date',
+			args: {
+				frequency: frm.doc.payroll_frequency,
+				start_date: frm.doc.start_date
+			},
+			callback: function (r) {
+				if (r.message) {
+					frm.set_value('end_date', r.message.end_date);
+				}
+			}
+		});
+	},
+	payroll_frequency: function (frm) {
+		frm.trigger("set_start_end_dates").then(() => {
+			// frm.events.clear_employee_table(frm);
+		});
+	},
+	// clear_employee_table: function (frm) {
+	// 	frm.clear_table('employees');
+	// 	frm.refresh();
+	// },
+	set_start_end_dates: function (frm) {
+		if (!false) {
+			frappe.call({
+				method: 'hrms.payroll.doctype.payroll_entry.payroll_entry.get_start_end_dates',
+				args: {
+					payroll_frequency: frm.doc.payroll_frequency,
+					start_date: frm.doc.posting_date
+				},
+				callback: function (r) {
+					if (r.message) {
+						in_progress = true;
+						frm.set_value('start_date', r.message.start_date);
+						frm.set_value('end_date', r.message.end_date);
+					}
+				}
+			});
+		}
+	},
+	currency: function (frm) {
+		var company_currency;
+		if (!frm.doc.company) {
+			company_currency = erpnext.get_currency(frappe.defaults.get_default("Company"));
+		} else {
+			company_currency = erpnext.get_currency(frm.doc.company);
+		}
+		if (frm.doc.currency) {
+			if (company_currency != frm.doc.currency) {
+				frappe.call({
+					method: "erpnext.setup.utils.get_exchange_rate",
+					args: {
+						from_currency: frm.doc.currency,
+						to_currency: company_currency,
+					},
+					callback: function (r) {
+						frm.set_value("exchange_rate", flt(r.message));
+						frm.set_df_property('exchange_rate', 'hidden', 0);
+						frm.set_df_property("exchange_rate", "description", "1 " + frm.doc.currency +
+							" = [?] " + company_currency);
+					}
+				});
+			} else {
+				frm.set_value("exchange_rate", 1.0);
+				frm.set_df_property('exchange_rate', 'hidden', 1);
+				frm.set_df_property("exchange_rate", "description", "");
+			}
+		}
+	},
+	setup: function (frm) {
+		frm.set_query('account', 'payroll_payable_table', function () {
+			return {
+				filters: {
+					"company": frm.doc.company,
+					"root_type": "Liability",
+					"is_group": 0,
+				}
+			};
+		});
 
-	// }
+	}
 });
--- shukhdev_customizations/shukhdev_customizations/doctype/employee_payroll_entry/employee_payroll_entry.py
+++ shukhdev_customizations/shukhdev_customizations/doctype/employee_payroll_entry/employee_payroll_entry.py
@@ -1,8 +1,23 @@
 # Copyright (c) 2023, Jigar Tarpara and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class EmployeePayrollEntry(Document):
-	pass
+	@frappe.whitelist()
+	def create_payroll_entry(self):
+		for row in self.payroll_payable_table:
+			self._create_payroll_entry(row)
+	
+	def _create_payroll_entry(self, row):
+		payroll_entry = frappe.new_doc("Payroll Entry")
+		payroll_entry.posting_date = self.posting_date
+		payroll_entry.payroll_frequency = self.payroll_frequency
+		payroll_entry.company = self.company
+		payroll_entry.currency = self.currency
+		payroll_entry.start_date = self.start_date
+		payroll_entry.end_date = self.end_date
+		payroll_entry.payroll_payable_account = row.account
+		payroll_entry.exchange_rate = 1
+		payroll_entry.save()
